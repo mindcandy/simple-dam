a7d7f72956e0befdbf976b3337536fd8774648b9
Mark Baker <markb@mindcandy.com>
2012-11-16 13:54:28 +0000

    initial version

---
+++ project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "simple-dam"
+  val appVersion      = "0.1.0"
+
+  val appDependencies = Seq(
+    "org.squeryl" %% "squeryl" % "0.9.5-2",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4",
+    "org.mockito" % "mockito-core" % "1.9.0"
+  )
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here      
+  )
+
+}
