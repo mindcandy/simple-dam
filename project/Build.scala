237448bc89ffe15193f9ca22c9e9bae6f9df59d1
Mark Baker <markb@mindcandy.com>
2012-11-19 11:18:08 +0000

    gzip static resources in stage/dist sbt build for faster loading

--- project/Build.scala
+++ project/Build.scala
@@ -8,13 +8,34 @@ object ApplicationBuild extends Build {
   val appVersion      = "0.1.0"
 
   val appDependencies = Seq(
-    // "org.squeryl" %% "squeryl" % "0.9.5-2",
-    // "postgresql" % "postgresql" % "9.1-901.jdbc4",
     "org.mockito" % "mockito-core" % "1.9.0"
   )
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    // Add your own project settings here      
+
+  // gzip all assets that we can
+  val gzippableAssets = SettingKey[PathFinder]("gzippable-assets", "Defines the files to gzip")
+  val gzipAssets = TaskKey[Seq[File]]("gzip-assets", "gzip all assets")
+
+  lazy val gzipAssetsSetting = gzipAssets <<= gzipAssetsTask dependsOn (copyResources in Compile)
+  lazy val gzipAssetsTask = (gzippableAssets, streams) map {
+    case (finder: PathFinder, s: TaskStreams) => {
+      var count = 0
+      var files = finder.get.map { file =>
+        val gzTarget = new File(file.getAbsolutePath + ".gz")
+        IO.gzip(file, gzTarget)
+        count += 1;
+        gzTarget
+      }
+      s.log.info("Compressed " + count + " asset(s)")
+      files
+    }
+  }
+
+  
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      gzippableAssets <<= (classDirectory in Compile)(dir => (dir ** ("*.js" || "*.css" || "*.html"))),
+      gzipAssetsSetting,
+      playPackageEverything <<= playPackageEverything dependsOn gzipAssets
   )
 
 }
