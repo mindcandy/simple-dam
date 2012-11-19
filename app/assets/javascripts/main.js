4fd69c60c4a50e0273b19e524b9516c541cfacd5
Mark Baker <markb@mindcandy.com>
2012-11-19 16:05:27 +0000

    making keyword tree more reliably open to the current folder by switching to the 'ui' plugin and making it do a search based on Selection rather than just clicking on a link :)

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,5 @@
 /* shared javascript */
 
-
-/**
- * add a hashCode to String so we can hash paths
- */
-String.prototype.hashCode = function(){
-    var hash = 0, i, ch;
-    if (this.length == 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        ch = this.charCodeAt(i);
-        hash = ((hash<<5)-hash)+ch;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-};
-
-
-
 /* set up function */
 jQuery(document).ready(function() {
 
