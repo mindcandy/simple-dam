7b737a495d0690b9258e6aeabbb0f2383de96753
Mark Baker <markb@mindcandy.com>
2012-12-10 11:42:42 +0000

    refactoring, moving on click event functions out of the big doc.ready() function for easier maintenance.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -19,6 +19,7 @@ var statusText = function(text) {
 
 var doSearch = function(searchType, searchParam, order) {
   // console.log("inner search: ", searchType, searchParam, order);
+  statusText("Searching...");
   LibraryUI.searchType = searchType;
   LibraryUI.searchParam = searchParam;
   LibraryUI.order = order;
@@ -335,145 +336,154 @@ var deselectAllAssets = function() {
   updateUiState(0);
 };
 
+var submitSearchForm = function(e) {
+  e.preventDefault();
+  var textSearch = $("#search").val();
+  if (textSearch) {
+   LibraryUI.searchAssets(textSearch);
+  }
+  return false;
+};
+
+var showEverythingClicked = function(e) {
+  LibraryUI.searchAssets('');
+};  
+
+var massEditMetaClicked = function(e) {
+  e.stopPropagation();
+  if (isBtnDisabled($(this))) return;    
+  // populate the list of assets to edit
+
+  var options = "";
+
+  $(".selectedAsset").each(function() {
+      options += '<option>' + $(this).attr("data-original") + '</option>';
+  });
+
+  $("#massEditAsset .btn").show(); 
+  $("#massEditAssetList").html("").append(options);
+  $("#massEditAsset").modal();
+};
+
+var massEditMetaSubmitClicked = function(e) {
+  e.stopPropagation();
+  if (isBtnDisabled($(this))) return;
+
+  // submit ajax query
+  var assets = $(".selectedAsset").map(function() { return $(this).attr('data-original'); }).toArray();
+  var queryParameters = {
+    "addKeywords": $("#addKeywords").val(),
+    "removeKeywords": $("#removeKeywords").val(),
+    "assets": assets
+  };
+
+  // prevent multiple submits
+  $("#massEditAsset .btn").hide(); 
+  $("#massEditMetaProgress").html("Working...");
+  
+  // send query
+  var reloadAfter = function() { location.reload(); }
+  postJson(jsRoutes.controllers.Admin.massEditMetadata(), queryParameters)
+  .done(reloadAfter).fail(reloadAfter);
+};
+
+var getAssetsToDownload = function() {
+  var assets = $(".selectedAsset");
+
+  if (assets.length === 0) {
+    // download all if no selection
+    return $(".inner-asset");
+  }
+  return assets;
+};
+
+var downloadAllButtonClicked = function(e) {
+  e.stopPropagation();
+  if (isBtnDisabled($(this))) return;
+
+  var assets = getAssetsToDownload();
+
+  if (assets.length === 0) {
+    alert("Nothing to download!");
+    return;
+
+  } else if (assets.length === 1) {
+    // exactly one asset, download it
+    assets.each(function() {
+      window.location.href = jsRoutes.controllers.FileServer.serve($(this).attr("data-original"));
+    });
+    return;
+
+  } else if (assets.length > AssetArchiveDownloadLimit) {
+
+    alert("Too many assets! You can only download " + AssetArchiveDownloadLimit + " at a time, apologies.");
+    return;
+  }
+
+  var options = "";
+  var totalSizeMB = 0.0;
+
+  assets.each(function() {
+      options += '<option>' + $(this).attr("data-original") + '</option>';
+      totalSizeMB += ($(this).attr("data-size-bytes") / (1024 * 1024));
+  });
+
+  // TODO: limit number or total size of files that can be downloaded in one go?
+  //       or provide warning?
+
+  totalSizeMB = Math.floor(0.5 + totalSizeMB);
+
+  $("#massDownloadAssetLabel").html(assets.length + " Assets, approx " + totalSizeMB + " MB before archiving");
+  $("#massDownload .btn").show(); 
+  $("#massDownloadAssetList").html("").append(options);
+  $("#massDownloadProgress").html("");
+  $("#massDownload").modal();
+};
+
+var downloadSubmitButtonClicked = function(e) {
+  e.stopPropagation();
+  if (isBtnDisabled($(this))) return;
+
+  // submit ajax query
+  var assets = getAssetsToDownload();
+  var assetPaths = assets.map(function() { return $(this).attr('data-original'); }).toArray();
+  var queryParameters = {     
+    "assets": assetPaths
+  };
+
+  // prevent multiple submits
+  $("#massDownload .btn").hide(); 
+  $("#massDownloadProgress").html("Building archive of " + assets.length + " assets, please wait...");
+  
+  // send query
+  postJson(jsRoutes.controllers.ArchiveBuilder.archive(), queryParameters)
+  .done(function(data) {
+      $('#massDownload').modal('hide');
+      window.location = data.archive;
+
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('#massDownload').modal('hide');
+      alert( "Archive Build failed: " + textStatus + " " + errorThrown);
+  });
+};
+
 
 // set up UI when its loaded - mainly onclick functions
 jQuery(document).ready(function() {
 
-  $("#searchForm").submit(function(e) {
-    e.preventDefault();
-    var textSearch = $("#search").val();
-    if (textSearch) {
-     LibraryUI.searchAssets(textSearch);
-    }
-    return false;
-  });
-
-  $("#everything").click(function(e) {
-    LibraryUI.searchAssets('');
-  });
+  $("#searchForm").submit(submitSearchForm);
+  $("#everything").click(showEverythingClicked);
 
   $("#selectAllBtn").click(selectAllAssets);
   $("#deselectAllBtn").click(deselectAllAssets);
 
   // Mass edit of assets
-  $("#massEditMetaBtn").click(function(e) {
-    e.stopPropagation();
-    if (isBtnDisabled($(this))) return;    
-    // populate the list of assets to edit
-
-    var options = "";
-
-    $(".selectedAsset").each(function() {
-        options += '<option>' + $(this).attr("data-original") + '</option>';
-    });
-
-    $("#massEditAsset .btn").show(); 
-    $("#massEditAssetList").html("").append(options);
-    $("#massEditAsset").modal();
-  });
-
-  $("#massEditMetaSubmitBtn").click(function(e) {
-    e.stopPropagation();
-    if (isBtnDisabled($(this))) return;
-
-    // submit ajax query
-    var assets = $(".selectedAsset").map(function() { return $(this).attr('data-original'); }).toArray();
-    var queryParameters = {
-      "addKeywords": $("#addKeywords").val(),
-      "removeKeywords": $("#removeKeywords").val(),
-      "assets": assets
-    };
-
-    // prevent multiple submits
-    $("#massEditAsset .btn").hide(); 
-    $("#massEditMetaProgress").html("Working...");
-    
-    // send query
-    var reloadAfter = function() { location.reload(); }
-    postJson(jsRoutes.controllers.Admin.massEditMetadata(), queryParameters)
-    .done(reloadAfter).fail(reloadAfter);
-  });
-
-  var getAssetsToDownload = function() {
-    var assets = $(".selectedAsset");
-
-    if (assets.length === 0) {
-      // download all if no selection
-      return $(".inner-asset");
-    }
-    return assets;
-  };
+  $("#massEditMetaBtn").click(massEditMetaClicked);
+  $("#massEditMetaSubmitBtn").click(massEditMetaSubmitClicked);
 
   // mass download of assets
-  $("#downloadAllBtn").click(function(e) {
-    e.stopPropagation();
-    if (isBtnDisabled($(this))) return;
-
-    var assets = getAssetsToDownload();
-
-    if (assets.length === 0) {
-      alert("Nothing to download!");
-      return;
-
-    } else if (assets.length === 1) {
-      // exactly one asset, download it
-      assets.each(function() {
-        window.location.href = jsRoutes.controllers.FileServer.serve($(this).attr("data-original"));
-      });
-      return;
-
-    } else if (assets.length > AssetArchiveDownloadLimit) {
-
-      alert("Too many assets! You can only download " + AssetArchiveDownloadLimit + " at a time, apologies.");
-      return;
-    }
-
-    var options = "";
-    var totalSizeMB = 0.0;
-
-    assets.each(function() {
-        options += '<option>' + $(this).attr("data-original") + '</option>';
-        totalSizeMB += ($(this).attr("data-size-bytes") / (1024 * 1024));
-    });
-
-    // TODO: limit number or total size of files that can be downloaded in one go?
-    //       or provide warning?
-
-    totalSizeMB = Math.floor(0.5 + totalSizeMB);
-
-    $("#massDownloadAssetLabel").html(assets.length + " Assets, approx " + totalSizeMB + " MB before archiving");
-    $("#massDownload .btn").show(); 
-    $("#massDownloadAssetList").html("").append(options);
-    $("#massDownloadProgress").html("");
-    $("#massDownload").modal();
-  });
-
-  $("#massDownloadSubmitBtn").click(function(e) {
-    e.stopPropagation();
-    if (isBtnDisabled($(this))) return;
-
-    // submit ajax query
-    var assets = getAssetsToDownload();
-    var assetPaths = assets.map(function() { return $(this).attr('data-original'); }).toArray();
-    var queryParameters = {     
-      "assets": assetPaths
-    };
-
-    // prevent multiple submits
-    $("#massDownload .btn").hide(); 
-    $("#massDownloadProgress").html("Building archive of " + assets.length + " assets, please wait...");
-    
-    // send query
-    postJson(jsRoutes.controllers.ArchiveBuilder.archive(), queryParameters)
-    .done(function(data) {
-        $('#massDownload').modal('hide');
-        window.location = data.archive;
-
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        $('#massDownload').modal('hide');
-        alert( "Archive Build failed: " + textStatus + " " + errorThrown);
-    });
-  });
+  $("#downloadAllBtn").click(downloadAllButtonClicked);
+  $("#massDownloadSubmitBtn").click(downloadSubmitButtonClicked);
 
 }); //jquery.docready
   
