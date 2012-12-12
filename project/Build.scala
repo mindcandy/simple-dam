02084955172769a24952105b5ed5607db5f1cfe5
Mark Baker <markb@mindcandy.com>
2012-12-12 13:07:11 +0000

    bump version number to 0.2.2

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
   val appName         = "simple-dam"
-  val appVersion      = "0.2.1"
+  val appVersion      = "0.2.2"
 
   val appDependencies = Seq(
     "org.mockito" % "mockito-core" % "1.9.0",
